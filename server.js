b2d8428604947d1a206e2abf9552b78590d38f4f
coupy <sidepupup11@yahoo.co.jp>
2013-04-14 14:47:25 +0900

    htmlよみこみ

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 ﻿var http = require('http');
 
 http.createServer(function (req, res) {
-  
+  fs.readFile('./HTMLPage.html', 'UTF-8', function(err, data) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Hello, world!');
+  res.end(data);
+  });
   
 }).listen(80);
