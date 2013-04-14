5ff1f725b9beaad8506a37e4a01d0f045876c9e6
coupy <sidepupup11@yahoo.co.jp>
2013-04-14 14:49:22 +0900

    いんでｘ

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ﻿var http = require('http');
 
 http.createServer(function (req, res) {
-  fs.readFile('./HTMLPage.html', 'UTF-8', function(err, data) {
+  fs.readFile('./index.html', 'UTF-8', function(err, data) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(data);
   });
