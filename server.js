b787234831d8ead3007f6cfd4d4c9fbbd46b2fde
coupy <sidepupup11@yahoo.co.jp>
2013-04-14 15:16:22 +0900

    serverjsしゅーせー

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ http.createServer(function (req, res) {
   
 });
 
-var io = require('socket.io').listen(app, { log: false });
+var io = require('socket.io').listen(http, { log: false });
 io.sockets.on('connection', function (socket) {
     socket.on('pulse', function (data) {
         socket.emit('pulse', data ? data * 2 : 0);
