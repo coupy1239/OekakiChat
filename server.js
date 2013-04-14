c626931ac4961904ff5cda869df7863196e9feb6
coupy <sidepupup11@yahoo.co.jp>
2013-04-14 14:41:31 +0900

    helloworldにしてみた

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-﻿var http = require('http')
-  , fs = require('fs');
+﻿var http = require('http');
 
 http.createServer(function (req, res) {
-
-  fs.readFile('./index.html', 'UTF-8', function(err, data) {
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);  // 「Hello, world!」から変更
-
-  });
-
+  
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('Hello, world!');
+  
 }).listen(process.env.PORT || 8080);
