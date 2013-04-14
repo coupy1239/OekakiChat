f0a213704bf824c19e916f707febd916f4caae83
coupy <sidepupup11@yahoo.co.jp>
2013-04-14 18:42:42 +0900

    app config package

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-﻿//var http = require('http'),fs = require('fs');
+﻿var http = require('http'),fs = require('fs');
 
-/*var app = http.createServer(function (req, res) {
+var app = http.createServer(function (req, res) {
   fs.readFile('./index.html', 'UTF-8', function(err, data) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(data);
   });
 });
-*/
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
-app.listen(8124);
- 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
 
-//app.listen(80);
+
+app.listen(80);
