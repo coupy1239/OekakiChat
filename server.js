96929c72e5a96adb47b47ec7d6430c2a8d7ae632
coupy <sidepupup11@yahoo.co.jp>
2013-04-14 18:22:26 +0900

    sendfile

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 ﻿var http = require('http'),fs = require('fs');
 
-var app = http.createServer(function (req, res) {
+/*var app = http.createServer(function (req, res) {
   fs.readFile('./index.html', 'UTF-8', function(err, data) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(data);
   });
-  
 });
-app.listen(80);
+*/
+
+var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
+app.listen(8124);
+ 
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+//app.listen(80);
