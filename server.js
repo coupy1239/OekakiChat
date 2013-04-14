eb6660047239d0caef841f9b194229b6296480e2
coupy <sidepupup11@yahoo.co.jp>
2013-04-14 19:17:50 +0900

    serverjs

--- server.js
+++ server.js
@@ -1,4 +1,34 @@
-﻿var http = require('http'),fs = require('fs');
+﻿var app = require('express').createServer()
+  , io = require('socket.io').listen(app);
+app.listen(8124);
+ 
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+ 
+io.sockets.on('connection', function (socket) {
+ 
+    // クライアントからメッセージ受信
+    socket.on('clear send', function () {
+ 
+        // 自分以外の全員に送る
+        socket.broadcast.emit('clear user');
+    });
+ 
+    // クライアントからメッセージ受信
+    socket.on('server send', function (msg) {
+ 
+        // 自分以外の全員に送る
+        socket.broadcast.emit('send user', msg);
+    });
+ 
+    // 切断
+    socket.on('disconnect', function () {
+        io.sockets.emit('user disconnected');
+    });
+});
+
+/*var http = require('http'),fs = require('fs');
 
 var app = http.createServer(function (req, res) {
   fs.readFile('./index.html', 'UTF-8', function(err, data) {
@@ -9,3 +39,4 @@ var app = http.createServer(function (req, res) {
 
 
 app.listen(80);
+*/
