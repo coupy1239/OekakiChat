1b856c56003175ff78c20091a9db20d23dfa7aa3
coupy <sidepupup11@yahoo.co.jp>
2013-04-14 18:51:59 +0900

    appjs check

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var app = require('express').createServer()
+var http = require('http'),fs = require('fs');
+
+var app = http.createServer(function (req, res) {
+  fs.readFile('./index.html', 'UTF-8', function(err, data) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(data);
+  });
+});
+
+app.listen(80);
+
+/*var app = require('express').createServer()
   , io = require('socket.io').listen(app);
 app.listen(8124);
  
@@ -27,3 +38,4 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('user disconnected');
     });
 });
+*/
