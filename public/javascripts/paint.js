b7dda867128b06ea38487740df9f1b01ac1c8d0a
coupy <sidepupup11@yahoo.co.jp>
2013-06-08 05:12:45 +0900

    色我那覇　直線

--- public/javascripts/paint.js
+++ public/javascripts/paint.js
@@ -5,7 +5,6 @@ jQuery(document).ready(function(){
   ////いろんなもの読み込む////
   
   var browser = navigator.userAgent;//ブラウザ
-  console.log(browser);
   
   var paint = new io.connect('/paint');//ソケット
   paint.on('paint points', function(data) {
@@ -20,9 +19,12 @@ jQuery(document).ready(function(){
   
   var randomID = Math.random();//クライアントID
   
+  var stampcanvas = document.getElementById('p0');//スタンプ用キャンバス
+  var ctxs = stampcanvas.getContext('2d');
+  
   var canvas = document.getElementById('p1');//描画キャンバス
   var context = canvas.getContext('2d');
-  context.fillStyle = "white";
+  context.fillStyle = 'rgba(255,255,255,255)';
   context.fillRect(0,0,canvas.width,canvas.height);
   context.lineWidth = 4;
   context.lineCap = 'round';
@@ -45,7 +47,7 @@ jQuery(document).ready(function(){
   
   var bufpts = new Array();  
   var brushsize = 4;  
-  var mycolor = 'black';
+  var mycolor = '#000000';
   var brushstyle = 'pen';
   var positioning = null;
   var drawing = false;
@@ -53,6 +55,7 @@ jQuery(document).ready(function(){
   var buffering = false;
   var clearing = false;
   var mouseout = false;
+  var shiftdown = false;
   
   ////////////
   
@@ -61,23 +64,16 @@ jQuery(document).ready(function(){
   
   mousecanvas.addEventListener('mousedown', function(event) {
     drawing = true;
-    if(brushstyle=='pen') drawArc(event,cs.style.backgroundColor);
-    if(brushstyle=='eraser') drawArc(event,'white');
+    if(brushstyle=='pen'){
+        if(shiftdown) drawLine(event,cs.style.backgroundColor);
+        else drawArc(event,cs.style.backgroundColor);
+    }
+    if(brushstyle=='eraser'){
+        if(shiftdown) drawLine(event,'white');
+        else drawArc(event,'white');
+    }
     if(brushstyle=='gnh'){
-        event.preventDefault();
-        positioning = position(event);
-        var points = {
-        s: 'stamp'
-      , x: positioning.x
-      , y: positioning.y
-      , w: brushsize
-      , id: canvas.id
-      , rid: randomID
-      , img:'gnh'
-        }
-        //paint.json.emit('paint points', points);
-        buffer(points);
-        painting(points); 
+         drawStamp(event,brushstyle,cs.style.backgroundColor);
     } 
     if(brushstyle=='spuit'){
         var imgdata = context.createImageData(1,1);
@@ -103,7 +99,9 @@ jQuery(document).ready(function(){
 
   mousecanvas.addEventListener('mouseup', function(event) {
     if (drawing == true) {
-      if(brushstyle=='pen') drawLine(event,cs.style.backgroundColor);
+      if(brushstyle=='pen') {
+          drawLine(event,cs.style.backgroundColor);
+      }
       if(brushstyle=='eraser') drawLine(event,'white');
       drawing = false;
     }
@@ -115,7 +113,6 @@ jQuery(document).ready(function(){
       positioning = position(event);
       if(brushstyle=='pen') drawLine(event,cs.style.backgroundColor);
       if(brushstyle=='eraser') drawLine(event,'white');
-      //drawing = false;
     }
     ctxm.clearRect(0, 0, mousecanvas.width, mousecanvas.height);
   }, false);
@@ -137,6 +134,13 @@ jQuery(document).ready(function(){
     ctxm.clearRect(0, 0, mousecanvas.width, mousecanvas.height);
   }, false);
   
+  document.addEventListener('keydown',function(event){
+    shiftdown = event.shiftKey;
+  }, false);
+  
+  document.addEventListener('keyup',function(event){
+    shiftdown = event.shiftKey;
+  }, false);
   ////////////////
   
   
@@ -170,7 +174,6 @@ jQuery(document).ready(function(){
         , url: canvas.toDataURL()
         , time: yyyymmddhhmiss()
       };
-      //paint.json.emit('paint points', points);
       buffer(points);
       painting(points);
     }
@@ -197,26 +200,72 @@ jQuery(document).ready(function(){
   
   var drawcursor = function(event){
         event.preventDefault();
-        var positions = position(event);    
+        var positions = position(event);
+        ctxm.clearRect(0, 0, mousecanvas.width, mousecanvas.height); 
         if (brushstyle == 'pen') {
-            ctxm.fillStyle = cs.style.backgroundColor;
-            ctxm.clearRect(0, 0, mousecanvas.width, mousecanvas.height);
+            ctxm.fillStyle = cs.style.backgroundColor;            
             ctxm.beginPath();
             ctxm.arc(positions.x, positions.y, brushsize / 2, 0, Math.PI * 2, true);
             ctxm.fill();
+            //直線表示
+            if(shiftdown && positioning){
+                ctxm.lineWidth = brushsize;
+                ctxm.strokeStyle = cs.style.backgroundColor;      
+                ctxm.beginPath();
+                ctxm.moveTo(positions.x, positions.y);   
+                ctxm.lineTo(positioning.x, positioning.y);
+                ctxm.closePath();
+                ctxm.stroke();
+            }
         }
         else if (brushstyle == 'eraser'){
+            ctxm.lineWidth = 1;
             ctxm.strokeStyle = 'black';
             ctxm.fillStyle = 'white';
-            ctxm.clearRect(0, 0, mousecanvas.width, mousecanvas.height);
             ctxm.beginPath();
             ctxm.arc(positions.x, positions.y, brushsize / 2, 0, Math.PI * 2, true);
             ctxm.stroke();
             ctxm.fill();
-        }
-        else if (brushstyle == 'gnh'){
-            ctxm.clearRect(0, 0, mousecanvas.width, mousecanvas.height);
-            ctxm.drawImage(imgarr['gnh'],positions.x-imgarr['gnh'].width*(0.5+brushsize/10)/2,positions.y-imgarr['gnh'].height*(0.5+brushsize/10)/2,imgarr['gnh'].width*(0.5+brushsize/10),imgarr['gnh'].height*(0.5+brushsize/10));
+            //直線表示
+            if(shiftdown && positioning){
+                ctxm.lineWidth = brushsize;
+                ctxm.strokeStyle = 'white';      
+                ctxm.beginPath();
+                ctxm.moveTo(positions.x, positions.y);   
+                ctxm.lineTo(positioning.x, positioning.y);
+                ctxm.closePath();
+                ctxm.stroke();
+            }
+        }                
+        else if (brushstyle == 'gnh') {
+            //ctxm.drawImage(imgarr['gnh'],positions.x-imgarr['gnh'].width*(0.5+brushsize/10)/2,positions.y-imgarr['gnh'].height*(0.5+brushsize/10)/2,imgarr['gnh'].width*(0.5+brushsize/10),imgarr['gnh'].height*(0.5+brushsize/10));
+        
+            ctxm.fillStyle = cs.style.backgroundColor;
+            var stampSize = {
+                x : imgarr['gnh'].width * (0.5 + brushsize / 10),
+                y : imgarr['gnh'].height * (0.5 + brushsize / 10)
+            };
+            var stampPosition = {
+                x : positions.x - stampSize.x / 2,
+                y : positions.y - stampSize.y / 2
+            };
+            ctxs.clearRect(0, 0, stampcanvas.width, stampcanvas.height);
+            ctxs.drawImage(imgarr['gnh'], stampPosition.x, stampPosition.y, stampSize.x, stampSize.y);//スタンプ用キャンバスに描画
+        
+            var stampData = ctxs.createImageData(stampSize.x, stampSize.y);
+            stampData = ctxs.getImageData(stampPosition.x, stampPosition.y, stampSize.x, stampSize.y);
+
+            var stamp_r = parseInt(ctxm.fillStyle.substring(1, 3), 16);
+            var stamp_g = parseInt(ctxm.fillStyle.substring(3, 5), 16);
+            var stamp_b = parseInt(ctxm.fillStyle.substring(5, 7), 16);
+            for (i in stampData.data) {
+                if (i % 4 == 3 && stampData.data[i] > 0) {
+                    stampData.data[i - 3] = stamp_r;
+                    stampData.data[i - 2] = stamp_g;
+                    stampData.data[i - 1] = stamp_b;
+                }
+            }
+            ctxm.putImageData(stampData, stampPosition.x, stampPosition.y);
         }
         else if (brushstyle == 'spuit'){
             
@@ -257,7 +306,24 @@ jQuery(document).ready(function(){
     }
     buffer(points)
     painting(points);
-    positioning = points;
+    positioning = positions;
+  }
+  
+  function drawStamp(event,img,color){
+        event.preventDefault();
+        positioning = position(event);
+        var points = {
+        s: 'stamp'
+      , x: positioning.x
+      , y: positioning.y
+      , w: brushsize
+      , c: color
+      , id: canvas.id
+      , rid: randomID
+      , img: img
+        }
+        buffer(points);
+        painting(points);
   }
 
   function painting(points) {
@@ -266,34 +332,56 @@ jQuery(document).ready(function(){
       context.lineWidth = points.w;
       switch (points.s) {
       case 'line':
-        context.strokeStyle = points.c;
-        context.fillStyle = points.c;        
+        context.strokeStyle = points.c;               
         context.beginPath();
         context.moveTo(points.x, points.y);   
         context.lineTo(points.xp, points.yp);
         context.closePath();
         context.stroke();
-        context.beginPath();
+        context.fillStyle = points.c; 
         context.arc(points.x, points.y, context.lineWidth/2, 0, Math.PI*2, true);
         context.fill();
         context.arc(points.xp, points.yp, context.lineWidth/2, 0, Math.PI*2, true);
         context.fill();
         break;
       case 'arc':
-        context.strokeStyle = points.c;
         context.fillStyle = points.c;
         context.beginPath();
         context.arc(points.x, points.y, context.lineWidth/2, 0, Math.PI*2, true);
         context.fill();
-        context.beginPath();
-        context.moveTo(points.x, points.y);
         break;
       case 'stamp':
-        context.drawImage(imgarr[points.img],points.x-imgarr['gnh'].width*(0.5+points.w/10)/2,points.y-imgarr['gnh'].height*(0.5+points.w/10)/2,imgarr['gnh'].width*(0.5+points.w/10),imgarr['gnh'].height*(0.5+points.w/10));
+        //旧スタンプ
+        //context.drawImage(imgarr[points.img],points.x-imgarr['gnh'].width*(0.5+points.w/10)/2,points.y-imgarr['gnh'].height*(0.5+points.w/10)/2,imgarr['gnh'].width*(0.5+points.w/10),imgarr['gnh'].height*(0.5+points.w/10));
+        
+        //新スタンプ        
+        context.fillStyle = points.c;
+        var stampSize = {x:imgarr[points.img].width*(0.5+points.w/10),y:imgarr[points.img].height*(0.5+points.w/10)};
+        var stampPosition = {x:points.x-stampSize.x/2,y:points.y-stampSize.y/2};
+        ctxs.clearRect(0, 0, stampcanvas.width, stampcanvas.height);
+        ctxs.drawImage(imgarr[points.img],stampPosition.x,stampPosition.y,stampSize.x,stampSize.y);//スタンプ用キャンバスに描画
+        
+        var stampData =ctxs.createImageData(stampSize.x,stampSize.y);
+        stampData = ctxs.getImageData(stampPosition.x,stampPosition.y,stampSize.x,stampSize.y);
+        var canvasData = context.createImageData(stampSize.x,stampSize.y);
+        canvasData = context.getImageData(stampPosition.x,stampPosition.y,stampSize.x,stampSize.y);
+        
+        var stamp_r = parseInt(context.fillStyle.substring(1,3),16);
+        var stamp_g = parseInt(context.fillStyle.substring(3,5),16);
+        var stamp_b = parseInt(context.fillStyle.substring(5,7),16);
+        for(i in stampData.data){
+            if(i%4 == 3 && stampData.data[i]>0){
+                var alpha = stampData.data[i]/255;
+                canvasData.data[i-3] = stamp_r*alpha + canvasData.data[i-3]*(1-alpha);
+                canvasData.data[i-2] = stamp_g*alpha + canvasData.data[i-2]*(1-alpha);
+                canvasData.data[i-1] = stamp_b*alpha + canvasData.data[i-1]*(1-alpha);
+            }
+        }
+        context.putImageData(canvasData,stampPosition.x,stampPosition.y);        
         break;
       case 'clear':
         clearing = true;
-        context.fillStyle = "rgb(255,255,255)";
+        context.fillStyle = "rgba(255,255,255,255)";
         context.fillRect(0,0,canvas.width,canvas.height);
         break;
 
@@ -307,16 +395,19 @@ jQuery(document).ready(function(){
       bufpts.push(points);
       
       if(points.s == 'clear'||points.s == 'save'){
+          buffering = true;
           emitting();
       }else if(!buffering){
           buffering = true;
-          setTimeout(function(){emitting()},500);          
+          setTimeout(function(){emitting()},500);
       }
   }
   function emitting(){
+      if(buffering){
           paint.json.emit('paint points', bufpts);
           bufpts = [];
           buffering = false;
+      }
   }
   
 }, false);
